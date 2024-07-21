aecb09b956dcfcd96842a4e800ceed348c3883b4
Eliya-jordanNinja <eliyay@jordanninja.onmicrosoft.com>
2024-07-22 01:13:44 +0300

    update

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Script to list all files from a GitHub directory using GitHub API with wget and awk (Limited Capability)
+# Script to list all files from a GitHub directory using GitHub API with wget and jq
 
+echo "Hello!"
 sudo apt update
-sudo apt install wget
+sudo apt install wget jq -y  # Install wget and jq
+
 echo "download!"
 FONT_DIR="$HOME/.local/share/fonts"
-mkdir -p $FONT_DIR
+mkdir -p "$FONT_DIR"
 
 REPO="eliyay010/fonts-installation"
 PATH="fonts/Calibri"
@@ -14,10 +16,10 @@ PATH="fonts/Calibri"
 API_URL="https://api.github.com/repos/$REPO/contents/$PATH"
 
 # Use wget to fetch the JSON data from GitHub API
-response=$(wget -qO- $API_URL)
+response=$(wget -qO- "$API_URL")
 
-# Parse JSON response with awk to extract file names
-echo $response
+# Parse JSON response with jq to extract file names
+echo "$response" | jq '.[] | .name'
 
 echo "Files listed successfully."
 echo "Press enter to exit"
