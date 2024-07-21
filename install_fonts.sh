ac3e0186dd7aed1d1f5161248b866cd2786dbb8d
Eliya-jordanNinja <eliyay@jordanninja.onmicrosoft.com>
2024-07-22 00:47:54 +0300

    update

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # Script to download and install fonts from a specific directory on a server
 
+FONT_DIR="$HOME/.local/share/fonts"
+mkdir -p $FONT_DIR
+
+SERVER_URL="wget -qO- https://raw.githubusercontent.com/eliyay010/fonts-installation/main/fonts/Calibri/"
+
+# Fetch the list of font files from the server
+echo "Listing font files from $SERVER_URL:"
+wget -qO- $SERVER_URL | grep -oP 'href="\K[^"]+' | grep '\.ttf$'
+
 echo "Fonts installed successfully."
 echo "Press enter to exit"
 read
